9feb6890dc2b570f5c35aaca26d34e05b8e0d18c
Arturo Mendoza <arturo.mendoza@quak.mx>
2015-02-18 13:28:26 -0600

    Works TransaccionDP

--- TransaccionDP.java
+++ TransaccionDP.java
@@ -2,27 +2,33 @@ import java.util.*;
 
 public class TransaccionDP
 {
-	private String nocta,nombre,tipo;
-	private int saldo;
+	private String nocta, tipo, fecha, hora;
+	private int saldoAnterior, cantidad, saldoActual;
 	
 	
 	/*** Constructores ***/
-	public ClienteDP()
+	public TransaccionDP()
 	{
 		this.nocta  = "";
-		this.nombre = "";
 		this.tipo   = "";
-		this.saldo  = 0;
+		this.saldoAnterior  = 0;
+		this.cantidad = 0;
+		this.saldoActual = 0;
+		this.fecha = "";
+		this.hora = "";
 	}
 	
-	public ClienteDP(String datos)
+	public TransaccionDP(String datos)
 	{
 		StringTokenizer st = new StringTokenizer(datos,"_");
 		
 		this.nocta  = st.nextToken();
-		this.nombre = st.nextToken();
 		this.tipo   = st.nextToken();
-		this.saldo  = Integer.parseInt(st.nextToken());
+		this.saldoAnterior  = Integer.parseInt(st.nextToken());
+		this.cantidad = Integer.parseInt(st.nextToken());
+		this.saldoActual = Integer.parseInt(st.nextToken());
+		this.fecha = st.nextToken();
+		this.hora = st.nextToken();
 	}
 	
 	/*** Accesors ***/
@@ -30,20 +36,35 @@ public class TransaccionDP
 	{
 		return this.nocta;
 	}
-	
-	public String getNombre()
-	{
-		return this.nombre;
-	}
-	
+		
 	public String getTipo()
 	{
 		return this.tipo;
 	}
 	
-	public int getSaldo()
+	public int getSaldoAnterior()
 	{
-		return this.saldo;
+		return this.saldoAnterior;
+	}
+	
+	public int getCantidad()
+	{
+		return this.cantidad;
+	}
+	
+	public int getSaldoActual()
+	{
+		return this.saldoAnterior;
+	}
+	
+	public String getFecha()
+	{
+		return this.fecha;
+	}
+	
+	public String getHora()
+	{
+		return this.hora;
 	}
 	
 	
@@ -52,28 +73,43 @@ public class TransaccionDP
 	{
 		this.nocta = str;
 	}
-	
-	public void setNombre(String str)
-	{
-		this.nombre = str;
-	}
-	
+		
 	public void setTipo(String str)
 	{
 		this.tipo = str;
 	}
 	
-	public void setSaldo(int n)
+	public void setSaldoAnterior(int n)
 	{
-		this.saldo = n;
+		this.saldoAnterior = n;
+	}
+	
+	public void setCantidad(int n)
+	{
+		this.cantidad = n;
+	}
+	
+	public void setSaldoActual(int n)
+	{
+		this.saldoActual = n;
+	}
+	
+	public void setFecha(String str)
+	{
+		this.fecha = str;
+	}
+	
+	public void setHora(String str)
+	{
+		this.hora = str;
 	}
 	
 	public String toString()
 	{
-		return this.nocta+"_"+this.nombre+"_"+this.tipo+"_"+this.saldo;
+		return this.nocta+"_"+this.tipo+"_"+this.saldoAnterior+"_"+this.cantidad+"_"+this.saldoActual+"_"+this.fecha+"_"+this.hora;
 	}
     
     public String toSQLString(){
-        return "'" + this.nocta + "','" + this.nombre+"','"+this.tipo+"',"+this.saldo+"";
+        return "'" + this.nocta + "','" + this.tipo+"','" + this.saldoAnterior + "'," + this.cantidad + "'," + this.saldoActual + "'," + this.fecha + "'," + this.hora;
     }
 }
